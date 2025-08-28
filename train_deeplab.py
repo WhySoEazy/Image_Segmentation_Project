e2a9b1656eb94fded75185223a8aaffe614b30bf
Nguyễn Triết <trietnguyen23012003@gmail.com>
2025-08-28 15:21:53 +0700

    update_training_loop

--- train_deeplab.py
+++ train_deeplab.py
@@ -1,9 +1,12 @@
-from torchvision.transforms import ToTensor, Normalize, Compose
+from torchvision.transforms import ToTensor, Normalize, Compose, Resize
 import numpy as np
-from VOC_seg_dataset import MyVOCSeg
 from torch.utils.data import DataLoader
 import torch
 from torch.optim import SGD
+from torchvision.datasets import VOCSegmentation
+from VOC_seg_dataset import MyVOCSeg
+import tqdm
+from tqdm import tqdm
 
 def train():
 
@@ -13,15 +16,19 @@ def train():
     num_epochs = 100
 
     transform = Compose([
+        Resize((224,224)),
         ToTensor(),
         Normalize(mean = [0.485, 0.456, 0.406] , std = [0.229, 0.224, 0.225])
     ])
+
+    target_transform = Resize((224,224))
 
     train_dataset = MyVOCSeg(root="data",
                              year="2012",
                              image_set="train",
                              download=False,
-                             transform=transform)
+                             transform=transform,
+                             target_transform=target_transform)
     
     train_dataloader = DataLoader(
         dataset=train_dataset,
@@ -34,7 +41,9 @@ def train():
     test_dataset = MyVOCSeg(root="data",
                              year="2012",
                              image_set="val",
-                             download=False,)
+                             download=False,
+                             transform=transform,
+                             target_transform=target_transform)
     
     test_dataloader = DataLoader(
         dataset=test_dataset,
@@ -43,12 +52,25 @@ def train():
         drop_last=False
     )
 
-    model = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_resnet50', pretrained=True)
+    model = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_resnet50', pretrained=True).to(device)
     optimizer = SGD(lr=1e-3 , momentum=0.9 , params=model.parameters())
+    criterion = torch.nn.CrossEntropyLoss()
 
     for epoch in range(num_epochs):
         model.train()
-        
-
+        progess_bar = tqdm(train_dataloader)
+        #Forward
+        for images , targets in progess_bar:
+            images = images.to(device)
+            targets = targets.to(device)
+            result = model(images)
+            output = result["out"]
+            loss = criterion(output , targets)
+            progess_bar.set_description("Epoch: {}/{} , Loss: {:0.4f}".format(epoch+1 , num_epochs , loss.item()))
+            #Backward
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
 if __name__ == "__main__":
     train()
