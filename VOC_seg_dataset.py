a042718b1297f2a38f6ab8c4ae6d14cf232b99e3
Nguyễn Triết <trietnguyen23012003@gmail.com>
2025-08-28 15:20:18 +0700

    update

--- VOC_seg_dataset.py
+++ VOC_seg_dataset.py
@@ -1,26 +1,24 @@
 from torchvision.datasets import VOCSegmentation
-from torchvision.transforms import ToTensor, Normalize, Compose
+from torchvision.transforms import ToTensor, Normalize, Compose, Resize
 import numpy as np
 
 class MyVOCSeg(VOCSegmentation):
-    def __init__(self , root , year , image_set , download , transform=None):
-        super().__init__(root, year, image_set, download)
-        self.classes = ['background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 
-                        'bus', 'car', 'cat', 'chair','cow', 'diningtable', 'dog', 'horse', 
-                        'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+    def __init__(self, root, year, image_set, download, transform=None, target_transform=None):
+        super().__init__(root, year, image_set, download, transform, target_transform)
+        self.classes = ['background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair',
+                        'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa',
+                        'train', 'tvmonitor']
 
-        self.transform = transform
 
-    def __getitem__(self , item):
-        image , target = super().__getitem__(item)
-        target = np.array(target)
-        target[target==255] = 0
-        if self.transform != None:
-            image = self.transform(image)
-        return image , target
+    def __getitem__(self, item):
+        image, target = super().__getitem__(item)
+        target = np.array(target, np.int64)
+        target[target == 255] = 0
+        return image, target
 
 if __name__ == "__main__":
     transform = Compose([
+        Resize((224,224)),
         ToTensor(),
         Normalize(mean = [0.485, 0.456, 0.406] , std = [0.229, 0.224, 0.225])
     ])
